8056fc5afae917bb706a438ab2a1f93b61f221ef
Dyrda Dmitriy <dyrda.dmitrij@mail.ru>
2019-09-29 19:58:57 +0300

    Testing_Lab3

---
+++ Testing_Lab3/Testing_Lab3/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Testing_Lab3
+{
+    public class Triangle
+    {
+        public bool IsTriangle(float a, float b, float c)
+        {
+            if(a>0 && b>0 && c>0)
+            {
+                if (a + b > c && b + c > a && c + a > b)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
