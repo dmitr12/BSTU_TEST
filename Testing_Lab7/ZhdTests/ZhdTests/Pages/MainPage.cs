72aea07b6deea1d048133564f4282ff210fd9def
Dyrda Dmitriy <dyrda.dmitrij@mail.ru>
2019-11-10 22:54:32 +0300

    not full

---
+++ Testing_Lab7/ZhdTests/ZhdTests/Pages/MainPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ZhdTests.Model;
+
+namespace ZhdTests.Pages
+{
+    class MainPage
+    {
+        IWebDriver driver;
+        [FindsBy(How=How.XPath, Using= "//INPUT[@class='slide_tcal tc1']")]
+        IWebElement DepartureCity { get; set; }
+        [FindsBy(How = How.XPath, Using = "//INPUT[@class='slide_tcal tc2']")]
+        IWebElement ArrivalCity { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//INPUT[@class='search_button']")]
+        IWebElement SearchButton { get; set; }
+
+        public MainPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+            this.driver = driver;
+        }
+
+        public ListTicketsPage InputRouteDateAndClickSearch(Route route)
+        {
+            DepartureCity.SendKeys(route.DepartureCity);
+            ArrivalCity.SendKeys(route.ArrivalCity);
+            SearchButton.Click();
+            return new ListTicketsPage(driver);
+        }
+
+    }
+}
---
+++ Testing_Lab7/ZhdTests/ZhdTests/ZhdTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using ZhdTests.Driver;
+using ZhdTests.Model;
+using ZhdTests.Pages;
+using ZhdTests.Services;
+
+namespace WebDriver
+{
+    [TestClass]
+    public class ZhdTests
+    {
+        [TestMethod]
+        public void FindTicketWithoutIndicateSeat()
+        {
+            DriverSingleton.GetDriver().Navigate().GoToUrl("https://zhd.online");
+            TypeTrainPage typeTrainPage = new MainPage(DriverSingleton.GetDriver())
+                     .InputRouteDateAndClickSearch(RouteCreator.WithAllProperties())
+                     .ClickBuyFirstTicket()
+                     .ClickTypeTrain();
+            Assert.AreEqual("Необходимо выбрать 1 место", typeTrainPage.TextException.Text);
+            DriverSingleton.CloseDriver();
+        }
+        [TestMethod]
+        public void TrueRouteDate()
+        {
+            DriverSingleton.GetDriver().Navigate().GoToUrl("https://zhd.online");
+            ListTicketsPage listTicketsPage = new MainPage(DriverSingleton.GetDriver())
+                 .InputRouteDateAndClickSearch(RouteCreator.WithAllProperties());
+            Assert.AreNotEqual("0", listTicketsPage.CountRoute.Text);
+            DriverSingleton.CloseDriver();
+        }
+    }
+}
