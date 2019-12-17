146554e0a3921dab29da82ef760058cdbec49c5e
dmitr12 <42980069+dmitr12@users.noreply.github.com>
2019-12-17 11:10:21 +0300

    Update TestDataReader.cs

--- Testing_Lab9/dotnet/GitHubAutomation/Services/TestDataReader.cs
+++ Testing_Lab9/dotnet/GitHubAutomation/Services/TestDataReader.cs
@@ -28,7 +28,7 @@ namespace GitHubAutomation.Services
        
         public static string GetData(string key)
         {
-            return ConfigFile.AppSettings.Settings[key]?.Value;
+            return ConfigFile.AppSettings.Settings[key].Value;
         }
     }
 }
